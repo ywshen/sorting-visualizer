af599ae895094d05d08ab0c58ecfebe60acb535f
ywshen <ywshen5002@gmail.com>
2020-10-22 15:19:15 -0400

    Turning numbers into bars

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -16,8 +16,8 @@ export default class SortingVisualizer extends React.Component {
 
     resetArray(){
         const array = [];
-        for (let i=0; i< 100; i++){
-            array.push(randomIntFromInterval(5,1000));
+        for (let i=0; i< 310; i++){
+            array.push(randomIntFromInterval(5, 730));
         }
         this.setState({array});
     }
@@ -26,14 +26,14 @@ export default class SortingVisualizer extends React.Component {
         const {array} = this.state;
     
         return (
-          <>
+          <div className="array-container">
             {array.map((value, idx) => (
               <div
-                className="array-bar" key={idx}>
-                    {value}
-                </div>
+                className="array-bar" 
+                key={idx}
+                style={{height: `${value}px`}}></div>
             ))}
-          </>
+          </div>
         );
       }
     }
