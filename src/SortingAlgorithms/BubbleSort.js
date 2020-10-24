a43b2e1399a97109e57ce6a2f35a56ee755c2081
ywshen <ywshen5002@gmail.com>
2020-10-24 17:36:23 -0400

    Adding animations to bubble sort function

--- src/SortingAlgorithms/BubbleSort.js
+++ src/SortingAlgorithms/BubbleSort.js
@@ -1,12 +1,24 @@
-function bubbleSort(array) {
+export function getBubbleSortAnimations(array){
+    const animations = [];
+    if (array.length <= 1) return array;
+    buubleSort(array, animations);
+    return animations;
+}
+
+function bubbleSort(
+    array,
+    animations,) {
     for (let i=0; i<array.length; i++) {
         for (let j=0; j<array.length-i; j++){
             if (array[j] > array[j+1]) {
+                animations.push([j, j+1]);
+                animations.push([j,j+1]);
                 let temp = array[j];
+                animations.push([j, array[j+1]])
+                animations.push([j+1, temp])
                 arry[j] = array[j+1];
                 array[j+1] = temp; 
             }
         }
     }
-    return array;
 }
